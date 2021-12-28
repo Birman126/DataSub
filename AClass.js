14387b2e0bf116a4c6f6e54a15d97ea6fbb75052
Birman <birman126@gmail.com>
2021-12-28 18:54:23 +0400

    format

--- AClass.js
+++ AClass.js
@@ -1,21 +1,18 @@
 class AClass {
-    #_numbers=[];
+    #_numbers = [];
     constructor(n) {
-        if (TypeOf(n) !== "number") {throw new Error("n is NaN");}//проверка что n - это число, если не число то выводить ошибку trowError
+        if (TypeOf(n) !== "number") { throw new Error("n is NaN"); }//проверка что n - это число, если не число то выводить ошибку trowError
         this.#_numbers = new Array(n); //массив из n элементов
         this.#fill();
- 
     }
- 
-    get numbers() {return this._numbers}
+    get numbers() { return this._numbers }
     #fill() { //метод fill можно вызывать только из класса AClass
         for (let i = 0; i <= _numbers.length; i++) {
             _numbers[i] =
-            Math.floor(Math.random() * 100) //случайное число от 0 до 99. Для больших чисел - долгое выполнение
+                Math.floor(Math.random() * 100) //случайное число от 0 до 99. Для больших чисел - долгое выполнение
         }
-        //заполняет массив Number случайными числами
+        //геттер заполняет массив Number случайными числами
     }
- 
     factorial() {
         function factorialFunction(n) { //функция факториала
             return (n != 1) ? n * factorialFunction(n - 1) : 1;
@@ -24,22 +21,16 @@ class AClass {
         //метод factorial можно вызывать из класса AClass и его дочерних классов
     }
 }
- 
 class Class1 extends AClass {
- 
-    sort (#_numbers) {
-let acc = Numbers [0];
-let sorted =[]
-for (let n=1; n<=#_numbers.length-1; n++){
-for (let j=1; j<=#_numbers.length-1; j++)
-if (Numbers[n] < acc) {acc=Numbers[n]}
-sorted.push(acc);
-    } //сортировка массива Numbers
-}
-
-
- 
+    sort(#_numbers) {
+        let acc = Numbers[0];
+        let sorted = []
+        for (let n = 1; n <= #_numbers.length - 1; n++) {
+            for (let j = 1; j <= #_numbers.length - 1; j++)
+                if (Numbers[n] < acc) { acc = Numbers[n] }
+            sorted.push(acc);
+        } //сортировка массива Numbers
+    }
 class Class2 extends AClass {
-
-sort () {return #_numbers.sort} //сортировка массива Numbers другим способом
+    sort() { return #_numbers.sort } //сортировка массива Numbers стандартным способом
 }
